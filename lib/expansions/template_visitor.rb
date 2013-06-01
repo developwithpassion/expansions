68ea8efae41f36ea135d57ac775423f45ce71c10
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-05-31 21:08:41 -0600

    Fixed the settings file load

--- lib/expansions/template_visitor.rb
+++ lib/expansions/template_visitor.rb
@@ -11,7 +11,7 @@ module Expansions
     def run_using(file_name)
       processor = processors.get_processor_for(file_name)
       generated_name = File.basename(file_name,File.extname(file_name))
-      load_settings_file(generated_name)
+      load_settings_file(File.dirname(file_name), generated_name)
       generated_name = generated_name.gsub(/\.dotfile/,"") 
       generated_name = ".#{generated_name}" if (/\.dotfile/ =~ file_name) 
       output = File.join(File.dirname(file_name),generated_name)
@@ -19,8 +19,8 @@ module Expansions
       processor.process(:input => file_name,:output => output)
     end
 
-    def load_settings_file(file_name)
-      settings_file = "#{file_name}.settings"
+    def load_settings_file(folder, file_name)
+      settings_file = File.join(folder, "#{file_name}.settings")
       if File.exist?(settings_file)
         load settings_file
       end
